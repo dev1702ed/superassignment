688dd7b74cba5a06946c3938aa7c62607ed74d2a
dev1702ed <d.bhirin@iitg.ac.in>
2024-03-17 18:04:13 +0530

    second commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,11 @@
 const pool = require("../db");
+
+exports.initiateTransaction = (req,res) => {
+    const trans_id = req.body.trans_id;
+    const customer_id = req.body.customer_id;
+    const status = req.body.status;
+    const amount = req.body.amount;
+    pool.query("INSERT INTO Transactions (trans_id, customer_id, status, amount) VALUES ($1, $2, $3, $4);",[trans_id, customer_id, status, amount], (error, results)=>{
+          if(error) throw error;
+    });
+}
--- router/routes.js
+++ router/routes.js
@@ -1,6 +1,7 @@
 const {Router} = require("express");
 const router = Router();
-const controller = require("./controllers/controller")
+const controller = require("../controllers/controller")
 
+router.get('/initiateTransaction',controller.initiateTransaction);
 
 module.exports = router;
